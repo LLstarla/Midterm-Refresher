d97df4b1ddc7f4567f7d80bda1bc96a393d4baed
LLstarla <lgentry1@engr-134-18.eastern.ewu.edu>
2015-02-09 14:02:11 -0800

    3. js

--- 3/js/scripts.js
+++ 3/js/scripts.js
@@ -2,3 +2,7 @@
 // REF: http://api.jquery.com/contains-selector
 // REF: http://api.jquery.com/addclass/
 // Extend into an array of explitives
+
+console.log($('.text:contains("fuck")').length);
+
+$('.text:contains("fuck")').addClass('bad');
